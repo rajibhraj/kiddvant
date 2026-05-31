6c19f3d2e32f4cf39bdc54d40cba57f039839dd1
Mubin33 <mubinulislam14@gmail.com>
2026-05-31 12:29:00 +0600

    up

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  allowedDevOrigins: ['192.168.50.130', '192.168.50.119'],
   /* config options here */
   images: {
     remotePatterns: [
